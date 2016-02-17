9a2d1e04788bdb62a86a62fb43872e342853e6ff
Hao <Hxu9@sheffield.ac.uk>
2016-02-17 23:07:56 +0000

    This add the part c

--- Lab2/src/Pizza/Order.java
+++ Lab2/src/Pizza/Order.java
@@ -6,10 +6,36 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JRadioButton;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JTextArea;
+import javax.swing.ButtonGroup;
 
 public class Order extends JFrame {
 
 	private JPanel contentPane;
+	private final Action action = new SwingAction();
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Launch the application.
@@ -35,8 +61,63 @@ public class Order extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLabel lblWelcomeToMost = new JLabel("Welcome to the most amazing pizza odering system");
+		
+		JRadioButton rdbtnThinBase = new JRadioButton("Thin base");
+		buttonGroup.add(rdbtnThinBase);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Deep pan");
+		buttonGroup.add(rdbtnNewRadioButton);
+		
+		JButton btnOrderFinished = new JButton("Order finished");
+		btnOrderFinished.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnOrderFinished.setText("Thanks for your order");
+			}
+		});
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(347, Short.MAX_VALUE)
+					.addComponent(rdbtnThinBase)
+					.addContainerGap())
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(347, Short.MAX_VALUE)
+					.addComponent(rdbtnNewRadioButton)
+					.addContainerGap())
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(103)
+					.addComponent(lblWelcomeToMost)
+					.addContainerGap(75, Short.MAX_VALUE))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap(172, Short.MAX_VALUE)
+					.addComponent(btnOrderFinished)
+					.addGap(163))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblWelcomeToMost)
+					.addGap(18)
+					.addComponent(rdbtnThinBase)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(rdbtnNewRadioButton)
+					.addPreferredGap(ComponentPlacement.RELATED, 126, Short.MAX_VALUE)
+					.addComponent(btnOrderFinished)
+					.addContainerGap())
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
-
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
 }
