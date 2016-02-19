5394fcbc2dfccddf944e48162779db397175623d
Hao <Hxu9@sheffield.ac.uk>
2016-02-19 16:25:36 +0000

    this is the end of tutorial

--- Lab2/src/Pizza/Order.java
+++ Lab2/src/Pizza/Order.java
@@ -10,6 +10,7 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.EnumSet;
 import java.awt.event.ActionEvent;
 import java.awt.FlowLayout;
 import java.awt.CardLayout;
@@ -31,6 +32,10 @@ import javax.swing.Action;
 import javax.swing.JTextArea;
 import javax.swing.ButtonGroup;
 import javax.swing.JCheckBox;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JCheckBoxMenuItem;
 
 public class Order extends JFrame {
 
@@ -59,13 +64,15 @@ public class Order extends JFrame {
 	 * Create the frame.
 	 */
 	public Order() {
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 900, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
 		JLabel lblWelcomeToMost = new JLabel("Welcome to the most amazing pizza odering system");
+	
 		
 		JRadioButton rdbtnThinBase = new JRadioButton("Thin base");
 		//rdbtnThinBase.setSelected(true);
@@ -80,55 +87,98 @@ public class Order extends JFrame {
 		JCheckBox chckbxOlive = new JCheckBox("Olive");
 		JButton btnOrderFinished = new JButton("Order finished");
 		JTextArea txtrYourOrderInfo = new JTextArea();
-		txtrYourOrderInfo.setText("Your order info");
+		txtrYourOrderInfo.setText("Your order info:");
+		EnumSet <PizzaTopping> nonVegan = EnumSet.of(
+				PizzaTopping.CHICKEN,
+				PizzaTopping.SAUSAGE,   
+				PizzaTopping.HAM,
+				PizzaTopping.PRAWNS,    
+				PizzaTopping.BACON,     
+				PizzaTopping.BEEF,
+				PizzaTopping.TUNA,      
+				PizzaTopping.SALAMI,    
+				PizzaTopping.TURKEY,
+				PizzaTopping.SALMON);
 		btnOrderFinished.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
+				
 				btnOrderFinished.setText("Thanks for your order");
 				String base = null;
-				String topping = null;
+				String topping="";
+				String topping1 = null,topping2=null,topping3=null,topping4 = null;
 				if (rdbtnThinBase.isSelected()) {
 					base = "Thin Base";
 				}
 				if (rdbtnNewRadioButton.isSelected()) {
 					base = "Deep Pan";
 				}
-				if (chckbxChess.isSelected()){
-					topping = "chicken";
+				if (chckbxChicken.isSelected()) {
+					topping = topping +"  "+PizzaTopping.class;
+					
 				}
-				txtrYourOrderInfo.setText("You have ordered:"+base+"with topping"+topping);
+				if (chckbxChess.isSelected()){
+					topping2 ="  Cheese";
+					topping = topping + topping2;
+				}
+				if (chckbxOlive.isSelected()){
+					topping3="  Olive";
+					topping = topping + topping3;
+				}
+				txtrYourOrderInfo.setText("You have ordered:"+base+" with topping:"+topping);
+				
+				
+			
 			}
+				
 		});
+		JTextArea txtrHereListsYour = new JTextArea();
+		txtrHereListsYour.setText("Here lists your options");
+		
+		JRadioButton rdbtnVegans = new JRadioButton("Vegans");
+		
+		JRadioButton rdbtnNonvegans = new JRadioButton("Non-vegans");
+		if (rdbtnNonvegans.isSelected()){
+		
+		}
+		
 		
 		
 		
 		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(chckbxChicken)
-							.addPreferredGap(ComponentPlacement.RELATED, 244, Short.MAX_VALUE)
-							.addComponent(rdbtnThinBase))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(chckbxChess)
-							.addPreferredGap(ComponentPlacement.RELATED, 244, Short.MAX_VALUE)
-							.addComponent(rdbtnNewRadioButton))
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGap(103)
 							.addComponent(lblWelcomeToMost))
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addContainerGap(160, Short.MAX_VALUE)
-							.addComponent(btnOrderFinished)
-							.addGap(31)
-							.addComponent(txtrYourOrderInfo, GroupLayout.PREFERRED_SIZE, 122, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPane.createSequentialGroup()
 							.addContainerGap()
-							.addComponent(chckbxOlive)))
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(chckbxChicken)
+										.addComponent(chckbxChess))
+									.addGap(119)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addComponent(rdbtnVegans)
+										.addComponent(rdbtnNonvegans)))
+								.addComponent(chckbxOlive))
+							.addGap(208)
+							.addComponent(txtrHereListsYour, GroupLayout.PREFERRED_SIZE, 162, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 156, Short.MAX_VALUE)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addComponent(rdbtnThinBase)
+								.addComponent(rdbtnNewRadioButton))))
 					.addContainerGap())
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(54)
+					.addComponent(txtrYourOrderInfo, GroupLayout.PREFERRED_SIZE, 341, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 317, Short.MAX_VALUE)
+					.addComponent(btnOrderFinished)
+					.addGap(61))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -137,20 +187,26 @@ public class Order extends JFrame {
 					.addComponent(lblWelcomeToMost)
 					.addGap(18)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(rdbtnThinBase)
-						.addComponent(chckbxChicken))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(rdbtnNewRadioButton)
-						.addComponent(chckbxChess))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(chckbxOlive)
-							.addPreferredGap(ComponentPlacement.RELATED, 100, Short.MAX_VALUE)
-							.addComponent(btnOrderFinished))
-						.addComponent(txtrYourOrderInfo, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap())
+							.addComponent(rdbtnThinBase)
+							.addGap(9)
+							.addComponent(rdbtnNewRadioButton))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(chckbxChicken)
+								.addComponent(rdbtnVegans))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(chckbxChess)
+								.addComponent(rdbtnNonvegans))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(chckbxOlive))
+						.addComponent(txtrHereListsYour, GroupLayout.PREFERRED_SIZE, 403, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(txtrYourOrderInfo, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnOrderFinished))
+					.addGap(27))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
